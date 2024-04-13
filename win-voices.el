c0fc052f66e2af4b6b915aedb7fb37037dd77f55
Robert Melton <robert@robertmelton.com>
2024-04-12 22:32:29 -0400

    Almost there

--- win-voices.el
+++ win-voices.el
@@ -1,16 +1,16 @@
 ;;; win-voices.el --- Define  Win tags  -*- lexical-binding: t; -*-
 ;; $Id: win-voices.el 6342 2009-10-20 19:12:40Z tv.raman.tv $
 ;; $Author: Dave $
-;; Description:  Module to set up Win voices and personalities
-;; Keywords: Voice, Personality, Win
+;; Description:  Module to set up Mac voices and personalities
+;; Keywords: Voice, Personality, Mac
 ;;;   LCD Archive entry:
 
 ;; LCD Archive Entry:
-;; ewinspeak| T. V. Raman |tv.raman.tv@gmail.com
-;; A speech interface to Ewins |
+;; emacspeak| T. V. Raman |tv.raman.tv@gmail.com
+;; A speech interface to Emacs |
 ;; 
 ;;  $Revision: 4532 $ |
-;; Location https://github.com/tvraman/ewinspeak
+;; Location https://github.com/tvraman/emacspeak
 ;; 
 
 ;;;   Copyright:
@@ -18,20 +18,20 @@
 ;; Copyright (C) 1995 -- 2024, T. V. Raman 
 ;; All Rights Reserved.
 ;; 
-;; This file is not part of GNU Ewins, but the same permissions apply.
+;; This file is not part of GNU Emacs, but the same permissions apply.
 ;; 
-;; GNU Ewins is free software; you can redistribute it and/or modify
+;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 ;; 
-;; GNU Ewins is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;; 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Ewins; see the file COPYING.  If not, write to
+;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
@@ -39,77 +39,77 @@
 
 
 ;;; Commentary:
-;; This module defines the various voices used in voice-lock mode by Win TTS.
+;; This module defines the various voices used in voice-lock mode by Mac TTS.
 
 ;;; Code:
 
 ;;  Required modules: 
 
 (eval-when-compile (require 'cl-lib))
-(require 'ewinspeak-preamble)           ;For `ems--fastload'.
+(require 'emacspeak-preamble)           ;For `ems--fastload'.
 (cl-declaim  (optimize  (safety 0) (speed 3)))
 
 ;;; win:
 ;;;###autoload
-(defun sharpwin ()
-  "Swiftwin TTS."
+(defun win ()
+  "Swiftmac TTS."
   (interactive)
   (win-configure-tts)
   (ems--fastload "voice-defs")
-  (dtk-select-server "sharpwin")
+  (dtk-select-server "win")
   (dtk-initialize))
 
 ;;;  Customizations:
 
-(defcustom sharpwin-default-speech-rate 0.5
-  "Default speech rate for C#sharpwin."
+(defcustom win-default-speech-rate 0.5
+  "Default speech rate for C#win."
   :group 'tts
   :type 'integer
   :set #'(lambda(sym val)
            (set-default sym val)
-           (when (string-match "sharpwin\\'"dtk-program)
+           (when (string-match "win\\'"dtk-program)
              (setq-default dtk-speech-rate val))))
 
 ;;;   voice table
 
-(defvar sharpwin-default-voice-string "[{voice systemDefault}]"
-  "Default Swiftwin tag for  default voice.")
+(defvar win-default-voice-string "[{voice systemDefault}]"
+  "Default Swiftmac tag for  default voice.")
 
-(defvar sharpwin-voice-table (make-hash-table)
-  "Association between symbols and strings to set Swiftwin  voices.
+(defvar win-voice-table (make-hash-table)
+  "Association between symbols and strings to set Swiftmac  voices.
 The string can set any voice parameter.")
 
-(defun sharpwin-define-voice (name command-string)
-  "Define a Swiftwin  voice named NAME.
+(defun win-define-voice (name command-string)
+  "Define a Swiftmac  voice named NAME.
 This voice will be set   by sending the string
 COMMAND-STRING to the TTS engine."
-  (cl-declare (special sharpwin-voice-table))
-  (puthash name command-string sharpwin-voice-table))
+  (cl-declare (special win-voice-table))
+  (puthash name command-string win-voice-table))
 
-(defun sharpwin-get-voice-command-internal  (name)
+(defun win-get-voice-command-internal  (name)
   "Retrieve command string for  voice NAME."
-  (cl-declare (special sharpwin-voice-table))
+  (cl-declare (special win-voice-table))
   (cond
    ((listp name)
-    (mapconcat #'sharpwin-get-voice-command name " "))
-   (t (or  (gethash name sharpwin-voice-table)
-           sharpwin-default-voice-string))))
+    (mapconcat #'win-get-voice-command name " "))
+   (t (or  (gethash name win-voice-table)
+           win-default-voice-string))))
 
-(defun sharpwin-get-voice-command (name)
+(defun win-get-voice-command (name)
   "Retrieve command string for  voice NAME."
-  (sharpwin-get-voice-command-internal name))
+  (win-get-voice-command-internal name))
 
-(defun sharpwin-voice-defined-p (name)
+(defun win-voice-defined-p (name)
   "Check if there is a voice named NAME defined."
-  (cl-declare (special sharpwin-voice-table))
-  (gethash name sharpwin-voice-table))
+  (cl-declare (special win-voice-table))
+  (gethash name win-voice-table))
 
 ;;;  voice definitions
 
 ;; the nine predefined voices: TODO: figure out if embedding is
 ;; possible (and update voice names).
 
-(sharpwin-define-voice 'paul  " [{voice systemDefault}] ")
+(win-define-voice 'paul  " [{voice systemDefault}] ")
 
 ;; Modified voices:
 
@@ -118,30 +118,30 @@ COMMAND-STRING to the TTS engine."
 
 ;;;  voice family codes
 
-(defun sharpwin-get-family-code (name)
+(defun win-get-family-code (name)
   "Get control code for voice family NAME."
-  (sharpwin-get-voice-command-internal name))
+  (win-get-voice-command-internal name))
 
 ;;;   hash table for mapping families to their dimensions
 
-(defvar sharpwin-css-code-tables (make-hash-table)
-  "Hash table holding vectors of sharpwin codes.
+(defvar win-css-code-tables (make-hash-table)
+  "Hash table holding vectors of win codes.
 Keys are symbols of the form <FamilyName-Dimension>.
 Values are vectors holding the control codes for the 10 settings.")
 
-(defun sharpwin-css-set-code-table (family dimension table)
+(defun win-css-set-code-table (family dimension table)
   "Set up voice FAMILY.
 Argument DIMENSION is the dimension being set,
 and TABLE gives the values along that dimension."
-  (cl-declare (special sharpwin-css-code-tables))
+  (cl-declare (special win-css-code-tables))
   (let ((key (intern (format "%s-%s" family dimension))))
-    (puthash key table sharpwin-css-code-tables)))
+    (puthash key table win-css-code-tables)))
 
-(defun sharpwin-css-get-code-table (family dimension)
+(defun win-css-get-code-table (family dimension)
   "Retrieve table of values for specified FAMILY and DIMENSION."
-  (cl-declare (special sharpwin-css-code-tables))
+  (cl-declare (special win-css-code-tables))
   (let ((key (intern (format "%s-%s" family dimension))))
-    (gethash key sharpwin-css-code-tables)))
+    (gethash key win-css-code-tables)))
 
 ;;;   average pitch
 
@@ -172,13 +172,13 @@ and TABLE gives the values along that dimension."
      (7 55)
      (8 58)
      (9 62)))
-  (sharpwin-css-set-code-table 'paul 'average-pitch table))
+  (win-css-set-code-table 'paul 'average-pitch table))
 
-(defun sharpwin-get-average-pitch-code (value family)
+(defun win-get-average-pitch-code (value family)
   "Get  AVERAGE-PITCH for specified VALUE and  FAMILY."
   (or family (setq family 'paul))
   (if value 
-      (aref (sharpwin-css-get-code-table family 'average-pitch)
+      (aref (win-css-get-code-table family 'average-pitch)
             value)
     ""))
 
@@ -209,13 +209,13 @@ and TABLE gives the values along that dimension."
      (7 98.7)
      (8  112.8)
      (9  127)))
-  (sharpwin-css-set-code-table 'paul 'pitch-range table))
+  (win-css-set-code-table 'paul 'pitch-range table))
 
-(defun sharpwin-get-pitch-range-code (value family)
+(defun win-get-pitch-range-code (value family)
   "Get pitch-range code for specified VALUE and FAMILY."
   (or family (setq family 'paul))
   (if value 
-      (aref (sharpwin-css-get-code-table family 'pitch-range)
+      (aref (win-css-get-code-table family 'pitch-range)
             value)
     ""))
 
@@ -245,12 +245,12 @@ and TABLE gives the values along that dimension."
      (7  1 1 70 .3)
      (8  1 1 80 .3)
      (9  1 1 90 .3)))
-  (sharpwin-css-set-code-table 'paul 'stress table))
+  (win-css-set-code-table 'paul 'stress table))
 
-(defun sharpwin-get-stress-code (value family)
+(defun win-get-stress-code (value family)
   (or family (setq family 'paul))
   (if value 
-      (aref (sharpwin-css-get-code-table family 'stress)
+      (aref (win-css-get-code-table family 'stress)
             value)
     ""))
 
@@ -258,47 +258,47 @@ and TABLE gives the values along that dimension."
 
 ;;;   paul richness TODO
 (let ((table (make-vector 10 "")))
-  (sharpwin-css-set-code-table 'paul 'richness table))
+  (win-css-set-code-table 'paul 'richness table))
 
-(defun sharpwin-get-richness-code (value family)
+(defun win-get-richness-code (value family)
   (or family (setq family 'paul))
   (if value 
-      (aref (sharpwin-css-get-code-table family 'richness)
+      (aref (win-css-get-code-table family 'richness)
             value)
     ""))
 
-;;;   sharpwin-define-voice-from-speech-style
+;;;   win-define-voice-from-speech-style
 
-(defun sharpwin-define-voice-from-speech-style (name style)
-  "Define NAME to be a sharpwin voice as specified by settings in STYLE."
+(defun win-define-voice-from-speech-style (name style)
+  "Define NAME to be a win voice as specified by settings in STYLE."
   (let* ((family(acss-family style))
          (command
           (concat 
-           (sharpwin-get-family-code family)
-           (sharpwin-get-average-pitch-code (acss-average-pitch style) family)
-           (sharpwin-get-pitch-range-code (acss-pitch-range style) family)
-           (sharpwin-get-stress-code (acss-stress style) family)
-           (sharpwin-get-richness-code (acss-richness style) family))))
-    (sharpwin-define-voice name command)))
+           (win-get-family-code family)
+           (win-get-average-pitch-code (acss-average-pitch style) family)
+           (win-get-pitch-range-code (acss-pitch-range style) family)
+           (win-get-stress-code (acss-stress style) family)
+           (win-get-richness-code (acss-richness style) family))))
+    (win-define-voice name command)))
 
 ;;;  Configurater 
 ;;;###autoload
-(defun sharpwin-configure-tts ()
-  "Configure TTS  to use sharpwin."
+(defun win-configure-tts ()
+  "Configure TTS  to use win."
   (cl-declare (special tts-default-speech-rate
-                       tts-default-voice sharpwin-default-speech-rate))
-  (setq tts-default-voice 'systemDefault)
-  (fset 'tts-voice-defined-p 'sharpwin-voice-defined-p)
-  (fset 'tts-get-voice-command 'sharpwin-get-voice-command)
-  (fset 'tts-define-voice-from-speech-style 'sharpwin-define-voice-from-speech-style)
-  (setq tts-default-speech-rate sharpwin-default-speech-rate)
-  (set-default 'tts-default-speech-rate sharpwin-default-speech-rate)
+                       tts-default-voice win-default-speech-rate))
+  (setq tts-default-voice "David")
+  (fset 'tts-voice-defined-p 'win-voice-defined-p)
+  (fset 'tts-get-voice-command 'win-get-voice-command)
+  (fset 'tts-define-voice-from-speech-style 'win-define-voice-from-speech-style)
+  (setq tts-default-speech-rate win-default-speech-rate)
+  (set-default 'tts-default-speech-rate win-default-speech-rate)
   (dtk-unicode-update-untouched-charsets
    '(ascii latin-iso8859-1 latin-iso8859-15 latin-iso8859-9
            eight-bit-graphic))
-  (setq esharpwinspeak-play-program nil))
+  (setq emacspeak-play-program nil))
 
-;;;  tts-env for Swiftwin:
+;;;  tts-env for Swiftmac:
 
-(provide 'sharpwin-voices)
+(provide 'win-voices)
 
